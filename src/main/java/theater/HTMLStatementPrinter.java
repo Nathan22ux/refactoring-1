ca3cc66f32ecd477a0e2dfd19ef74ceb7c8e1c1e
Nathan <nathanallen668@gmail.com>
2025-11-24 16:21:34 -0500

    commented out task 3 calsses and fixed statment printer

--- src/main/java/theater/HTMLStatementPrinter.java
+++ src/main/java/theater/HTMLStatementPrinter.java
@@ -1,33 +1,35 @@
-package theater;
+//package theater;
+//
+//import java.util.Map;
+//
+///**
+// * Generates HTML-formatted statements for theater invoices.
+// */
+//public class HTMLStatementPrinter extends StatementPrinter {
+//
+//    public HTMLStatementPrinter(Invoice invoice, Map<String, Play> plays) {
+//        super(invoice, plays);
+//    }
+//
+//    @Override
+//    public String statement() {
+//        final StatementData statementData = new StatementData(getInvoice(), getPlays());  // Add 'final'
+//        final StringBuilder result = new StringBuilder(String.format("<h1>Statement for %s</h1>%n",
+//                statementData.getCustomer()));
+//        result.append("<table>").append(System.lineSeparator());
+//        result.append(String.format(" <caption>Statement for %s</caption>%n", statementData.getCustomer()));
+//        result.append(" <tr><th>play</th><th>seats</th><th>cost</th></tr>").append(System.lineSeparator());
+//        for (Performance perfData : statementData.getPerformances()) {
+//            result.append(String.format(" <tr><td>%s</td><td>%s</td><td>%s</td></tr>%n",
+//                    statementData.getPlayName(perfData),
+//                    perfData.getAudience(),
+//                    usd(statementData.getAmount(perfData))));
+//        }
+//        result.append("</table>").append(System.lineSeparator());
+//        result.append(String.format("<p>Amount owed is <em>%s</em></p>%n", usd(statementData.getTotalAmount())));
+//        result.append(String.format("<p>You earned <em>%s</em> credits</p>%n", statementData.getVolumeCredits()));
+//        return result.toString();
+//    }
+//}
+//COMMENTED OUT TP NOT CAUSE MARKUS EROROR
 
-import java.util.Map;
-
-/**
- * Generates HTML-formatted statements for theater invoices.
- */
-public class HTMLStatementPrinter extends StatementPrinter {
-
-    public HTMLStatementPrinter(Invoice invoice, Map<String, Play> plays) {
-        super(invoice, plays);
-    }
-
-    @Override
-    public String statement() {
-        final StatementData statementData = new StatementData(getInvoice(), getPlays());  // Add 'final'
-        final StringBuilder result = new StringBuilder(String.format("<h1>Statement for %s</h1>%n",
-                statementData.getCustomer()));
-        result.append("<table>").append(System.lineSeparator());
-        result.append(String.format(" <caption>Statement for %s</caption>%n", statementData.getCustomer()));
-        result.append(" <tr><th>play</th><th>seats</th><th>cost</th></tr>").append(System.lineSeparator());
-        for (Performance perfData : statementData.getPerformances()) {
-            result.append(String.format(" <tr><td>%s</td><td>%s</td><td>%s</td></tr>%n",
-                    statementData.getPlayName(perfData),
-                    perfData.getAudience(),
-                    usd(statementData.getAmount(perfData))));
-        }
-        result.append("</table>").append(System.lineSeparator());
-        result.append(String.format("<p>Amount owed is <em>%s</em></p>%n", usd(statementData.getTotalAmount())));
-        result.append(String.format("<p>You earned <em>%s</em> credits</p>%n", statementData.getVolumeCredits()));
-        return result.toString();
-    }
-}
--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -22,32 +22,77 @@ public class StatementPrinter {
      * @throws RuntimeException if one of the play types is not known
      */
     public String statement() {
-        final StatementData statementData = new StatementData(invoice, plays);
-        return renderPlainText(statementData);
-    }
+        final StringBuilder result = new StringBuilder(String.format("Statement for %s%n",
+                invoice.getCustomer()));
 
-    private String renderPlainText(StatementData statementData) {
-        final StringBuilder result =
-                new StringBuilder("Statement for " + statementData.getCustomer() + System.lineSeparator());
-
-        for (Performance performance : statementData.getPerformances()) {
-            result.append(String.format(
-                    "  %s: %s (%s seats)%n",
-                    statementData.getPlayName(performance),
-                    usd(statementData.getAmount(performance)),
+        for (Performance performance : invoice.getPerformances()) {
+            result.append(String.format("  %s: %s (%s seats)%n",
+                    getPlay(performance).getName(),
+                    usd(getAmount(performance)),
                     performance.getAudience()));
         }
 
-        final int totalAmount = statementData.getTotalAmount();
-        final int volumeCredits = statementData.getVolumeCredits();
-
-        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
-        result.append(String.format("You earned %s credits%n", volumeCredits));
+        result.append(String.format("Amount owed is %s%n", usd(getTotalAmount())));
+        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
         return result.toString();
+    }
+
+    private Play getPlay(Performance performance) {
+        return plays.get(performance.getPlayID());
+    }
+
+    private int getAmount(Performance performance) {
+        int result;
+        final Play play = getPlay(performance);
+        switch (play.getType()) {
+            case "tragedy":
+                result = Constants.TRAGEDY_BASE_AMOUNT;
+                if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                    result += Constants.TRAGEDY_OVER_THRESHOLD_AMOUNT
+                            * (performance.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                }
+                break;
+            case "comedy":
+                result = Constants.COMEDY_BASE_AMOUNT;
+                if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                    result += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
+                            + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                }
+                result += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
+                break;
+            default:
+                throw new RuntimeException(String.format("unknown type: %s", play.getType()));
+        }
+        return result;
+    }
+
+    private int getVolumeCredits(Performance performance) {
+        int result = Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        if ("comedy".equals(getPlay(performance).getType())) {
+            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return result;
     }
 
     protected String usd(int amount) {
         return NumberFormat.getCurrencyInstance(Locale.US).format(amount / Constants.PERCENT_FACTOR);
+    }
+
+    private int getTotalAmount() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            result += getAmount(performance);
+        }
+        return result;
+    }
+
+    private int getTotalVolumeCredits() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            result += getVolumeCredits(performance);
+        }
+        return result;
     }
 
     protected Invoice getInvoice() {
